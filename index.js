34afdab7e8b6baa2df785413d03ec7a72089dc93
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2020-09-09 11:59:42 +0200

    Stoping the timer

--- index.js
+++ index.js
@@ -12,13 +12,24 @@ class Timer {
     this.tick();
     this.interval = setInterval(this.tick, 1000);
   };
-
-  tick = () => {
-    console.log('tick');
-  };
   pause = () => {
     clearInterval(this.interval);
   };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
 }
 
 const durationInput = document.querySelector('#duration');
